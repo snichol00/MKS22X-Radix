6d1c7208032bf8baf95e60cac7389ee142c93b50
Sophie Nichol <snichol00@stuy.edu>
2019-04-02 06:33:10 -0400

    started putting in buckets

--- Radix.java
+++ Radix.java
@@ -20,15 +20,23 @@ public class Radix{
     return 0;
   }
   public void rsort(int[]data){
+    @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     MyLinkedList<Integer> nums = new MyLinkedList;
     //initialize buckets
     for(int i = 0; i < buckets.length; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
-    for (int d = 0; d < max(data); d++){
+    int digits = max(data);
+    for (int d = 0; d <= digits; d++){
       for(int i = 0; i < data.length; i++){
-
+        int number = current.remove(0);
+        int dig = (int)(number / (Math.pow(10, i - 1))) % 10;
+        buckets[dig].add(number);
+      }
+      MyLinkedList<Integer> temp = buckets[0];
+      for (int i = 0; i < 10; y++) {
+        temp.extend(buckets[i]);
       }
     }
   }
