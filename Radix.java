33ed6c96299c71e440d37df95c378788a6081a45
Sophie Nichol <snichol00@stuy.edu>
2019-04-01 20:19:41 -0400

    minor changes

--- Radix.java
+++ Radix.java
@@ -4,24 +4,32 @@ public class Radix{
   }
 
   public int max(int[]data){
+    //find max
     int max = 0;
     for (i = 0; i < data.length; i++){
       if (data[i] > max){
         max = data[i];
       }
     }
+    //find number of digits in max
     for (x = 1; x > 0; x++){
-      if (max % pow(10, x) < 0){
+      if (max % pow(10, x) <= 0){
         return x;
       }
     }
     return 0;
   }
   public void rsort(int[]data){
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     MyLinkedList<Integer> nums = new MyLinkedList;
-    for (d = 0; d < max(data); d++){
+    //initialize buckets
+    for(int i = 0; i < buckets.length; i++){
+      buckets[i] = new MyLinkedList<Integer>();
+    }
+    for (int d = 0; d < max(data); d++){
+      for(int i = 0; i < data.length; i++){
 
+      }
     }
   }
 }
