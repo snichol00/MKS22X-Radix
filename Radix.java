b538ff389800f8ac6904900572112b47d085dc57
Sophie Nichol <snichol00@stuy.edu>
2019-04-02 10:11:47 -0400

    differentiating between array and Linked List

--- Radix.java
+++ Radix.java
@@ -22,22 +22,28 @@ public class Radix{
   public void rsort(int[]data){
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    MyLinkedList<Integer> nums = new MyLinkedList;
     //initialize buckets
     for(int i = 0; i < buckets.length; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
-    int digits = max(data);
-    for (int d = 0; d <= digits; d++){
-      for(int i = 0; i < data.length; i++){
-        int number = current.remove(0);
-        int dig = (int)(number / (Math.pow(10, i - 1))) % 10;
-        buckets[dig].add(number);
-      }
-      MyLinkedList<Integer> temp = buckets[0];
-      for (int i = 0; i < 10; y++) {
-        temp.extend(buckets[i]);
-      }
+    //first digit copies from array
+    for(int i = 0; i < data.length; i++){
+      //get digit from number
+      int dig = (int)(data[i] / (Math.pow(10, i - 1))) % 10;
+      //add to correct bucket
+      buckets[dig].add(data[i]);
     }
+    //link lists in new linked list
+    MyLinkedList<Integer> temp = buckets[0];
+    for (int i = 0; i < 20; y++) {
+      temp.extend(buckets[i]);
+    }
+    int digits = max(data);
+    //looping through digits
+    for (int d = 1; d <= digits; d++){
+      //looping through numbers
+      while(temp.next() != void){
+        
+      }
   }
 }
