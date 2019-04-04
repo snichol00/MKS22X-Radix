34ded802849243d37207ddc9ec33fdb39c5f3977
Sophie Nichol <snichol00@stuy.edu>
2019-04-03 22:35:57 -0400

    deleted unecessary parts

--- Node.java
+++ Node.java
@@ -1,37 +1,31 @@
-public class Node{
-  private int data;
-  private Node next,prev;
+public class Node<E>{
+  private E data;
+  private Node<E> next,prev;
 
   //Constructor
-  public Node(int myData, Node nextNode, Node prevNode) {
+  public Node(E myData, Node<E> nextNode, Node<E> prevNode) {
     data = myData;
     next = nextNode;
     prev = prevNode;
   }
 
+  public boolean hasNext(){
+    if(next == null){
+      return false;
+    }
+    return true;
+  }
+
   //Accessors
-  public int data(){
+  public E getData(){
     return data;
   }
 
-  public Node next(){
+  public Node<E> next(){
     return next;
   }
 
   public Node prev(){
     return prev;
-  }
-
-  //Mutators
-  public void setData(int value){
-    data = value;
-  }
-
-  public void setNext(Node nextNode){
-    next = nextNode;
-  }
-
-  public void setPrev(Node prevNode){
-    prev = prevNode;
   }
 }
