8eece6403238c17b0d66c7b739912d98b2480dbb
Sophie Nichol <snichol00@stuy.edu>
2019-04-03 22:52:27 -0400

    linked lists togther and copied back

--- Radix.java
+++ Radix.java
@@ -13,25 +13,39 @@ public class Radix{
       //add to correct bucket
       buckets[dig + 9].add(data[i]);
     }
+    //temp to store values
+    MyLinkedList<Integer> temp = new MyLinkedList<Integer>();
     //goes through rest of digits
     int digits = max(data);
     //looping through digits
     for (int d = 1; d <= digits; d++){
-      Node<Integer> node = data.start();
+      Node<Integer> node = temp.start();
       int num = node.getData();
       int dig = num / (int)(Math.pow(10, i)) % 10;
       buckets[dig + 9].add(num);
-      //looping through numbers
-      while(buckets.hasNext() != void){
+      //looping through and placing numbers in bucket
+      while(node.hasNext() != void){
+        // gets the digit
         dig = num / (int)(Math.pow(10, i)) % 10;
         node = node.next();
         num = node.getData();
+        // adds num to appropriate bucket
         buckets[dig + 9].add(num);
       }
     }
-    MyLinkedList<Integer> temp = new MyLinkedList<Integer>();
     for (int i = 0; i < 20; y++) {
       temp.extend(buckets[i]);
+    }
+    Node<Integer> node = data.start();
+    //set first value
+    data[0] = node.getData();
+    //keeps track of index for data to loop through
+    int idx = 1;
+    //loop through linked list and copy into data
+    while(node.hasNext() != void){
+      node = node.next();
+      data[i] = node.getData();
+      i++;
     }
   }
 
